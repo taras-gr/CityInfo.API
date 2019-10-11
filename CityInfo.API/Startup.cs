7a241e548acf676ba4c00c8ce8ef9eac1b7c952d
taras-gr <tar.grytsenko@gmail.com>
2019-10-11 16:01:03 +0300

    HI

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -27,7 +27,7 @@ namespace CityInfo.API
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello World!"); 
             });
         }
     }
